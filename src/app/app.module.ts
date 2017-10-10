f5092dcd4ec39a2d3d07e20964def778aa1a5462
logonrm <logonrm@fiap.com.br>
2017-10-09 21:16:24 -0300

    Init Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+/* Módulos */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RoutingModule } from './routing/routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+/* Componentes */
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    AddComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
